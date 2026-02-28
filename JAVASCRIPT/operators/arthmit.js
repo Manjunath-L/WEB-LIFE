2b8331b3c4ee3039d84b3b1e290cd690a088719e
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-02-28 18:41:24 +0530

    JavaScript

---
+++ JAVASCRIPT/operators/arthmit.js
@@ -0,0 +1,33 @@
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+
+
+console.log(a%b);
+console.log(b%a);
+console.log(-a%b);
+console.log(a%-b);
+
+console.log(b**a);
+console.log(2**4);
+
+
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a%=2);
+
+let c,d,e ;
+c = d = e = 10;
+console.log(c,d,e);
+
+
+
+
+
---
+++ JAVASCRIPT/varibles/script.js
@@ -0,0 +1,48 @@
+// var number;
+
+// number = 10;
+
+// console.log(number);
+
+// var number = 10;
+
+// number = 20;
+// console.log(number);
+
+// let number ;
+// number = 10
+// console.log(number);
+
+// let number = 10;
+
+// const number = 10;
+// console.log(number);
+
+// let string = `a man is good`
+// console.log(string);
+// console.log(typeof(string));
+
+// let str2 = `A man is good ${string}`;
+// console.log(str2);
+
+let data = undefined;
+console.log(data);
+console.log(typeof data);
+
+let data1 = Math.pow(2, 53);
+console.log(data1);
+console.log(typeof data1);
+
+let data2 = 10n;
+console.log(data2);
+console.log(typeof data2);
+
+document.writeln(data1);
+
+let symbole = Symbol("user");
+let symbole1 = Symbol("user");
+console.log(symbole == symbole1);
+console.log(typeof(symbole));
+
+
+
