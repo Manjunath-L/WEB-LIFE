41b67eb4f8ac978362c8fb69451e3fc57a054f7c
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-03-11 10:16:46 +0530

    JavaScript

---
+++ JAVASCRIPT/async/intor_GEC.js
@@ -0,0 +1,19 @@
+console.log("the value of x = ", x);
+var x = 10;
+console.log("the value of x = ", x);
+
+x = 30
+console.log("the value of x = ", x);
+x = 20
+console.log("the value of x = ", x);
+var x = 10;
+console.log("the value of x = ", x);
+
+greet();
+
+function greet() {
+  console.log("hello");
+  console.log("the value of x = ", x);
+  var x = 10;
+  console.log("the value of x = ", x);
+}   
--- JAVASCRIPT/functions/function.js
+++ JAVASCRIPT/functions/function.js
@@ -1,47 +1,108 @@
-function greet1() {
-  //? function def
-  console.log("Welcom to js"); //! function body
+// function greet1() {
+//   //? function def
+//   console.log("Welcom to js"); //! function body
+// }
+
+// greet1(); //? function calling
+
+// function greet(name) {
+//   //? function def
+//   console.log("Welcom to js " + name); //! function body
+// }
+
+// greet(); //? function calling
+// greet("manju"); //? function calling
+
+// //
+// function addnumber(a, b) {
+//   console.log(a + b);
+// }
+
+// addnumber(10, 20);
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let res = add(10, 20);
+// console.log(res * 399);
+
+// let functionName = function () {
+//   console.log("hi"); // function expression
+// };
+// functionName();
+
+// const arrow = (a) => a + 10;
+// arrow(12);
+// console.log(arrow(12));
+
+// let result = (a) => a + 200;
+// console.log(result(200));
+
+// let arrowfun = (a, b) => a + b;
+// console.log(arrowfun(10, 20));
+
+// let arrowname = (a) => console.log(a);
+// arrowname("manju");
+
+// function greet(fun) {
+//   console.log("hello");
+//   // console.log(fun);
+//   fun();
+// }
+
+// greet(function task() {
+//   console.log("bay");
+// });
+
+// console.log("High order function");
+
+// function greet(fun) {
+//   console.log("hello");
+//   // console.log(fun);
+//   fun();
+// }
+
+// greet(task);
+
+// function task() {
+//   console.log("bay");
+// }
+
+// console.log("High order function");
+
+// function greet() {
+//   console.log("hello");
+//   return function () {
+//     console.log("bay");
+//   };
+// }
+
+// // greet()();
+
+// let result = greet();
+// result();
+
+function orderFood(call_back, name, takeaway) {
+  console.log("////////////////////Welcome to star food////////////////////");
+  console.log("choose your order");
+  call_back(name);
+  takeaway();
 }
 
-greet1(); //? function calling
-
-function greet(name) {
-  //? function def
-  console.log("Welcom to js " + name); //! function body
+function vegSpical(name) {
+  console.log("Your Panner friderice is ready !!!!!!!!!");
+  console.log(name + " Bill 125 Rs/-");
 }
 
-greet(); //? function calling
-greet("manju"); //? function calling
-
-function addnumber(a, b) {
-  console.log(a + b);
+function nonvegSpical(name) {
+  console.log("Your chicken friderice is ready !!!!!!!!!");
+  console.log(name + " Bill 255 Rs/-");
 }
 
-addnumber(10, 20);
-
-function add(a, b) {
-  return a + b;
+function takeaway() {
+  console.log("plase collect the order on next gate");
 }
 
-let res = add(10, 20);
-console.log(res * 399);
-
-
-
-let functionName = function () {
-  console.log("hi"); // function expression
-};
-functionName();
-
-const arrow = (a) => a + 10;
-arrow(12);
-console.log(arrow(12));
-
-let result = (a) => a + 200;
-console.log(result(200));
-
-let arrowfun = (a, b) => a + b;
-console.log(arrowfun(10, 20));
-
-let arrowname = (a) => console.log(a);
-arrowname("manju");
+orderFood(vegSpical, "Manju", takeaway);
+orderFood(nonvegSpical, "Varun", takeaway);
