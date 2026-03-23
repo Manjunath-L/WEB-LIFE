80fe4c4a41ba7880864d983d1a3c1f18b1d99a40
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-03-23 18:20:42 +0530

    django

---
+++ JAVASCRIPT/strings/script.js
@@ -0,0 +1,35 @@
+// let str1 = "Roman Rings";
+
+// console.log(str1);
+// console.log(typeof(str1));
+
+// //* string interpullation ${}
+
+// let str2 = `the bird is female`;
+
+// console.log(`the string is ${str2}`);
+
+// let str3 = '345678909876543';
+// console.log(str3);
+
+// let str4 = new String("hello")
+// console.log(str4);
+// console.log(typeof(str4));
+
+// str4[0] = "hello";
+// console.log(str4);
+
+// let str = "Hello";
+
+// console.log(str);
+
+// str.replace("H","U")
+
+// // str[0] = "u"
+
+// console.log(str);
+
+let str2 = "Manjunth L";
+console.log(str2);
+console.log(str2.length);
+
