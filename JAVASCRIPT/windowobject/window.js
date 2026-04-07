fccbc66aeb4b79b81737ac462ddfaa90f6089a8b
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-04-07 12:40:31 +0530

    js 07-04-2026

---
+++ JAVASCRIPT/windowobject/window.js
@@ -0,0 +1,33 @@
+// console.log(window);
+// console.log(window.document)
+
+// window.alert("Hello World");
+
+// window.confirm("Are you sure?");
+
+// window.prompt("What is your name?");
+
+// var x = window.innerWidth;
+
+// var y = window.innerHeight;
+
+// console.log("Width: " + x + ", Height: " + y);
+
+// window.open("https://www.example.com", "_blank");
+
+// window.close();
+
+// var myWindow = window.open("", "myWindow", "width=200,height=100");
+// myWindow.document.write("<p>This is a new window!</p>")
+
+// let data = document.getElementById("main");
+// console.log(data);
+
+// let data1 = document.getElementsByClassName("main");
+// console.log(data1[1]);
+
+let data = document.getElementsByTagName("div");
+console.log(data);
+
+let data2 = document.getElementsByTagName("html");
+console.log(data2);
